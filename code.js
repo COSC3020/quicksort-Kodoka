f69f338a862e7b776ce125b4454a11e643ead32e
Kodoka <10721764+Kodoka@users.noreply.github.com>
2025-03-24 21:09:30 -0600

    Update code.js
    Push to pull

--- code.js
+++ code.js
@@ -1,3 +1,23 @@
-function quicksort(array) {
+function quicksort(array)
+{
+    var low = 0;
+    var high = array.length;
+    // Low equals or exceeds high, we're done, otherwise...
+    if(low < high)
+    {
+        // Partition and swap
+    }
     return array;
+}
+
+function partition(array, low, high)
+{
+
+}
+
+function swap(array, low, high)
+{
+    var temp = array[low];
+    array[low] = array[high];
+    array[high] = temp;
 }
